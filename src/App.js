1a146c50ba1aa4ce18a94a55f1dfee50144009fa
Aleph0Null0 <aryahosseini.contact@gmail.com>
2021-10-27 14:44:40 -0400

    Simple React Router Implementation
    Modifies to 3 allow navigation between 3 different pages (blank,doctors,patients) in "App.js"

--- src/App.js
+++ src/App.js
@@ -2,15 +2,41 @@ import logo from "./logo.svg";
 import "./App.css";
 import DoctorsList from "./components/DoctorsList";
 import PatientsList from "./components/PatientsList";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-function App(props) {
+export default function App(props) {
   return (
     <>
-      <h1>Hello, {props.name}</h1>
-      <DoctorsList />
-      <PatientsList />
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Homepage</Link>
+              </li>
+              <li>
+                <Link to="/doctors">Doctors</Link>
+              </li>
+              <li>
+                <Link to="/patients">Patients</Link>
+              </li>
+            </ul>
+          </nav>
+          <Switch>
+            <Route path="/doctors">
+              <DoctorsList />
+            </Route>
+            <Route path="/patients">
+              <PatientsList />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </>
   );
 }
-
-export default App;
