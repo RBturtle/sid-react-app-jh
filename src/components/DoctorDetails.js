442ac7ed8db89d0beedb7735fcfe0cf148b265a0
Joonha Lee <joonhalee15@gmail.com>
2021-10-28 15:39:54 -0400

    changed dob, specialty, address, street city province postal code in doctor details to fit new names in doctor info

--- src/components/DoctorDetails.js
+++ src/components/DoctorDetails.js
@@ -1,15 +1,15 @@
 import React from "react";
 
-const DoctorDetails = ({ scientific_name, dob, specialty, address }) => {
-  const { street, city, province, postal_code } = address;
+const DoctorDetails = ({ scientific_name, family_ancestor, interesting_fact, edible_food }) => {
+  const { edible_1, edible_2, edible_3, edible_4 } = edible_food;
 
   return (
     <>
-      <h5>Testing - name: {scientific_name}</h5>
-      <h5>Date of Birth: {dob}</h5>
-      <h5>Specialty: {specialty}</h5>
+      <h5>scientific_name: {scientific_name}</h5>
+      <h5>family_ancestor: {family_ancestor}</h5>
+      <h5>interesting_fact: {interesting_fact}</h5>
       <h5>
-        Address: {street}, {city}, {province} {postal_code}
+        edible_foods: {edible_1}, {edible_2}, {edible_3} {edible_4}
       </h5>
     </>
   );
