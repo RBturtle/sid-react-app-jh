03c5407a3565f3733d21f1f88c5f997dc29d3e3f
Joonha Lee <joonhalee15@gmail.com>
2021-10-28 19:49:51 -0400

    added lived year to details frontend

--- src/components/DoctorDetails.js
+++ src/components/DoctorDetails.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const DoctorDetails = ({ scientific_name, family_ancestor, interesting_fact, edible_food }) => {
+const DoctorDetails = ({ scientific_name, family_ancestor, interesting_fact, lived_year, edible_food }) => {
   const { edible_1, edible_2, edible_3, edible_4 } = edible_food;
 
   return (
@@ -8,6 +8,7 @@ const DoctorDetails = ({ scientific_name, family_ancestor, interesting_fact, edi
       <h5>scientific_name: {scientific_name}</h5>
       <h5>family_ancestor: {family_ancestor}</h5>
       <h5>interesting_fact: {interesting_fact}</h5>
+      <h5>lived year: {lived_year}</h5>
       <h5>
         edible_foods: {edible_1}, {edible_2}, {edible_3} {edible_4}
       </h5>
