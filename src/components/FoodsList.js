5544692fc3bbe1d263255c20d097112ff7fea664
ResiaG <73526633+ResiaG@users.noreply.github.com>
2021-11-01 12:40:45 +0800

    Update FoodsList.js

--- src/components/FoodsList.js
+++ src/components/FoodsList.js
@@ -6,8 +6,8 @@ const FoodsList = () => {
 
   const fetchFoods = () => {
     // fetch("http://localhost:4000/v1/foods")
-    // fetch(`https://salty-fortress-39510.herokuapp.com/v1/patients`) // doctor exercise node
-    fetch(`https://ghostly-witch-92778.herokuapp.com/sa/foods`)
+    fetch(`https://salty-fortress-39510.herokuapp.com/v1/patients`) // doctor exercise node
+    // fetch(`https://ghostly-witch-92778.herokuapp.com/sa/foods`)
       .then((response) => response.json())
       .then((result) => setFoods(result));
   };
